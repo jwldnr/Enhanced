2cd6871ee3ed3b59144aeec2b62b7741118a1e99
Jean Alexander Woldner <alexander@woldner.io>
2016-07-25 19:14:30 +0200

    format code

--- Enhanced.lua
+++ Enhanced.lua
@@ -26,8 +26,8 @@ function Addon:Load()
   do
     local eventHandler = CreateFrame('Frame', nil);
     eventHandler:SetScript('OnEvent', function(handler, ...)
-      self:OnEvent(...);
-    end)
+        self:OnEvent(...);
+      end)
 
     eventHandler:RegisterEvent('ADDON_LOADED');
     eventHandler:RegisterEvent('PLAYER_LOGIN');
@@ -44,8 +44,8 @@ function Addon:SetUpNamePlateFrame(frame)
     frame.castBar.background:SetVertexColor(0.0, 0.0, 0.0, 0.2);
     frame.castBar:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar");
 
-    frame.castBar:SetStatusBarColor(1.0, 0.7, 0.0);
-    frame.castBar.SetStatusBarColor = function () end
+    --frame.castBar:SetStatusBarColor(1.0, 0.7, 0.0);
+    --frame.castBar.SetStatusBarColor = function () end
   end
 
   SetNamePlateHealthValue(frame);
@@ -55,23 +55,23 @@ function Addon:SetUpNamePlateFrame(frame)
   end
 
   -- if (NamePlatePlayerResourceFrame) then
-  --   local _, class = UnitClass('player');
-  --   local namePlatePlayer = C_NamePlate.GetNamePlateForUnit('player');
+  -- local _, class = UnitClass('player');
+  -- local namePlatePlayer = C_NamePlate.GetNamePlateForUnit('player');
   --
-  --   if (namePlatePlayer) then
-  --     if (class == 'DEATHKNIGHT') then
-  --       DeathKnightResourceOverlayFrame:Hide();
-  --       DeathKnightResourceOverlayFrame.Show = function () end
+  -- if (namePlatePlayer) then
+  -- if (class == 'DEATHKNIGHT') then
+  -- DeathKnightResourceOverlayFrame:Hide();
+  -- DeathKnightResourceOverlayFrame.Show = function () end
   --
-  --       RuneFrame:SetParent(namePlatePlayer.UnitFrame);
-  --       RuneFrame:ClearAllPoints();
-  --       RuneFrame:SetPoint('CENTER', namePlatePlayer.UnitFrame.healthBar, 'CENTER', 3, -36);
-  --     end
-  --   end
+  -- RuneFrame:SetParent(namePlatePlayer.UnitFrame);
+  -- RuneFrame:ClearAllPoints();
+  -- RuneFrame:SetPoint('CENTER', namePlatePlayer.UnitFrame.healthBar, 'CENTER', 3, -36);
+  -- end
+  -- end
   --
-  --   if (class == 'DEATHKNIGHT') then
-  --     RuneFrame:SetShown(namePlatePlayer ~= nil);
-  --   end
+  -- if (class == 'DEATHKNIGHT') then
+  -- RuneFrame:SetShown(namePlatePlayer ~= nil);
+  -- end
   -- end
 end
 
@@ -124,6 +124,11 @@ function Addon:UpdateNamePlateCastingBarTimer(frame, elapsed)
     else
       frame.update = frame.update - elapsed;
     end
+  end
+
+  local r, g, b = frame:GetStatusBarColor();
+  if (r > 0.7 and g > 0.7 and b > 0.7) then
+    frame:SetStatusBarColor(0.5, 0.5, 0.5);
   end
 end
 
@@ -238,187 +243,188 @@ function Addon:PLAYER_LOGIN()
   self:HookActionEvents();
 
   for i, v in pairs({
-    -- unit frames
-    PlayerFrameTexture,
-    TargetFrameTextureFrameTexture,
-    PetFrameTexture,
-    PartyMemberFrame1Texture,
-    PartyMemberFrame2Texture,
-    PartyMemberFrame3Texture,
-    PartyMemberFrame4Texture,
-    PartyMemberFrame1PetFrameTexture,
-    PartyMemberFrame2PetFrameTexture,
-    PartyMemberFrame3PetFrameTexture,
-    PartyMemberFrame4PetFrameTexture,
-    FocusFrameTextureFrameTexture,
-    TargetFrameToTTextureFrameTexture,
-    FocusFrameToTTextureFrameTexture,
-    BonusActionBarFrameTexture0,
-    BonusActionBarFrameTexture1,
-    BonusActionBarFrameTexture2,
-    BonusActionBarFrameTexture3,
-    BonusActionBarFrameTexture4,
-    Boss1TargetFrameTextureFrameTexture,
-    Boss2TargetFrameTextureFrameTexture,
-    Boss3TargetFrameTextureFrameTexture,
-    Boss4TargetFrameTextureFrameTexture,
-    Boss5TargetFrameTextureFrameTexture,
-    Boss1TargetFrameSpellBarBorder,
-    Boss2TargetFrameSpellBarBorder,
-    Boss3TargetFrameSpellBarBorder,
-    Boss4TargetFrameSpellBarBorder,
-    Boss5TargetFrameSpellBarBorder,
-    CastingBarFrameBorder,
-    FocusFrameSpellBarBorder,
-    TargetFrameSpellBarBorder,
-    RuneButtonIndividual1BorderTexture,
-    RuneButtonIndividual2BorderTexture,
-    RuneButtonIndividual3BorderTexture,
-    RuneButtonIndividual4BorderTexture,
-    RuneButtonIndividual5BorderTexture,
-    RuneButtonIndividual6BorderTexture,
-    PaladinPowerBarBG,
-    PaladinPowerBarBankBG,
-    -- main menu
-    SlidingActionBarTexture0,
-    SlidingActionBarTexture1,
-    MainMenuBarLeftEndCap,
-    MainMenuBarRightEndCap,
-    MainMenuBarTexture0,
-    MainMenuBarTexture1,
-    MainMenuBarTexture2,
-    MainMenuBarTexture3,
-    MainMenuMaxLevelBar0,
-    MainMenuMaxLevelBar1,
-    MainMenuMaxLevelBar2,
-    MainMenuMaxLevelBar3,
-    MainMenuXPBarTextureLeftCap,
-    MainMenuXPBarTextureRightCap,
-    MainMenuXPBarTextureMid,
-    MainMenuXPBarDiv1,
-    MainMenuXPBarDiv2,
-    MainMenuXPBarDiv3,
-    MainMenuXPBarDiv4,
-    MainMenuXPBarDiv5,
-    MainMenuXPBarDiv6,
-    MainMenuXPBarDiv7,
-    MainMenuXPBarDiv8,
-    MainMenuXPBarDiv9,
-    MainMenuXPBarDiv10,
-    MainMenuXPBarDiv11,
-    MainMenuXPBarDiv12,
-    MainMenuXPBarDiv13,
-    MainMenuXPBarDiv14,
-    MainMenuXPBarDiv15,
-    MainMenuXPBarDiv16,
-    MainMenuXPBarDiv17,
-    MainMenuXPBarDiv18,
-    MainMenuXPBarDiv19,
-    ReputationWatchBarTexture0,
-    ReputationWatchBarTexture1,
-    ReputationWatchBarTexture2,
-    ReputationWatchBarTexture3,
-    ReputationXPBarTexture0,
-    ReputationXPBarTexture1,
-    ReputationXPBarTexture2,
-    ReputationXPBarTexture3,
-    ReputationWatchBar.StatusBar.WatchBarTexture0,
-    ReputationWatchBar.StatusBar.WatchBarTexture1,
-    ReputationWatchBar.StatusBar.WatchBarTexture2,
-    ReputationWatchBar.StatusBar.WatchBarTexture3,
-    StanceBarLeft,
-    StanceBarMiddle,
-    StanceBarRight,
-    -- arena frames
-    ArenaEnemyFrame1Texture,
-    ArenaEnemyFrame2Texture,
-    ArenaEnemyFrame3Texture,
-    ArenaEnemyFrame4Texture,
-    ArenaEnemyFrame5Texture,
-    ArenaEnemyFrame1SpecBorder,
-    ArenaEnemyFrame2SpecBorder,
-    ArenaEnemyFrame3SpecBorder,
-    ArenaEnemyFrame4SpecBorder,
-    ArenaEnemyFrame5SpecBorder,
-    ArenaEnemyFrame1PetFrameTexture,
-    ArenaEnemyFrame2PetFrameTexture,
-    ArenaEnemyFrame3PetFrameTexture,
-    ArenaEnemyFrame4PetFrameTexture,
-    ArenaEnemyFrame5PetFrameTexture,
-    ArenaPrepFrame1Texture,
-    ArenaPrepFrame2Texture,
-    ArenaPrepFrame3Texture,
-    ArenaPrepFrame4Texture,
-    ArenaPrepFrame5Texture,
-    ArenaPrepFrame1SpecBorder,
-    ArenaPrepFrame2SpecBorder,
-    ArenaPrepFrame3SpecBorder,
-    ArenaPrepFrame4SpecBorder,
-    ArenaPrepFrame5SpecBorder,
-    -- panes
-    CharacterFrameTitleBg,
-    CharacterFrameBg,
-    ObjectiveTrackerBlocksFrame.QuestHeader.Background,
-    -- minimap
-    MinimapBorder,
-    MinimapBorderTop,
-    MiniMapTrackingButtonBorder,
-    MiniMapLFGFrameBorder,
-    MiniMapBattlefieldBorder,
-    MiniMapMailBorder,
-    -- raid frame
-    CompactRaidFrameContainerBorderFrameBorderTopLeft,
-    CompactRaidFrameContainerBorderFrameBorderTop,
-    CompactRaidFrameContainerBorderFrameBorderTopRight,
-    CompactRaidFrameContainerBorderFrameBorderRight,
-    CompactRaidFrameContainerBorderFrameBorderBottomRight,
-    CompactRaidFrameContainerBorderFrameBorderBottom,
-    CompactRaidFrameContainerBorderFrameBorderBottomLeft,
-    CompactRaidFrameContainerBorderFrameBorderLeft,
-    select(1, MinimapZoomIn:GetRegions()),
-    select(3, MinimapZoomIn:GetRegions()),
-    select(1, MinimapZoomOut:GetRegions()),
-    select(3, MinimapZoomOut:GetRegions()),
-    QueueStatusMinimapButtonBorder,
-    select(1, TimeManagerClockButton:GetRegions())
-  }) do
+      -- unit frames
+      PlayerFrameTexture,
+      TargetFrameTextureFrameTexture,
+      PetFrameTexture,
+      PartyMemberFrame1Texture,
+      PartyMemberFrame2Texture,
+      PartyMemberFrame3Texture,
+      PartyMemberFrame4Texture,
+      PartyMemberFrame1PetFrameTexture,
+      PartyMemberFrame2PetFrameTexture,
+      PartyMemberFrame3PetFrameTexture,
+      PartyMemberFrame4PetFrameTexture,
+      FocusFrameTextureFrameTexture,
+      TargetFrameToTTextureFrameTexture,
+      FocusFrameToTTextureFrameTexture,
+      BonusActionBarFrameTexture0,
+      BonusActionBarFrameTexture1,
+      BonusActionBarFrameTexture2,
+      BonusActionBarFrameTexture3,
+      BonusActionBarFrameTexture4,
+      Boss1TargetFrameTextureFrameTexture,
+      Boss2TargetFrameTextureFrameTexture,
+      Boss3TargetFrameTextureFrameTexture,
+      Boss4TargetFrameTextureFrameTexture,
+      Boss5TargetFrameTextureFrameTexture,
+      Boss1TargetFrameSpellBarBorder,
+      Boss2TargetFrameSpellBarBorder,
+      Boss3TargetFrameSpellBarBorder,
+      Boss4TargetFrameSpellBarBorder,
+      Boss5TargetFrameSpellBarBorder,
+      CastingBarFrameBorder,
+      FocusFrameSpellBarBorder,
+      TargetFrameSpellBarBorder,
+      RuneButtonIndividual1BorderTexture,
+      RuneButtonIndividual2BorderTexture,
+      RuneButtonIndividual3BorderTexture,
+      RuneButtonIndividual4BorderTexture,
+      RuneButtonIndividual5BorderTexture,
+      RuneButtonIndividual6BorderTexture,
+      PaladinPowerBarBG,
+      PaladinPowerBarBankBG,
+      -- main menu
+      SlidingActionBarTexture0,
+      SlidingActionBarTexture1,
+      MainMenuBarLeftEndCap,
+      MainMenuBarRightEndCap,
+      MainMenuBarTexture0,
+      MainMenuBarTexture1,
+      MainMenuBarTexture2,
+      MainMenuBarTexture3,
+      MainMenuMaxLevelBar0,
+      MainMenuMaxLevelBar1,
+      MainMenuMaxLevelBar2,
+      MainMenuMaxLevelBar3,
+      MainMenuXPBarTextureLeftCap,
+      MainMenuXPBarTextureRightCap,
+      MainMenuXPBarTextureMid,
+      MainMenuXPBarDiv1,
+      MainMenuXPBarDiv2,
+      MainMenuXPBarDiv3,
+      MainMenuXPBarDiv4,
+      MainMenuXPBarDiv5,
+      MainMenuXPBarDiv6,
+      MainMenuXPBarDiv7,
+      MainMenuXPBarDiv8,
+      MainMenuXPBarDiv9,
+      MainMenuXPBarDiv10,
+      MainMenuXPBarDiv11,
+      MainMenuXPBarDiv12,
+      MainMenuXPBarDiv13,
+      MainMenuXPBarDiv14,
+      MainMenuXPBarDiv15,
+      MainMenuXPBarDiv16,
+      MainMenuXPBarDiv17,
+      MainMenuXPBarDiv18,
+      MainMenuXPBarDiv19,
+      ReputationWatchBarTexture0,
+      ReputationWatchBarTexture1,
+      ReputationWatchBarTexture2,
+      ReputationWatchBarTexture3,
+      ReputationXPBarTexture0,
+      ReputationXPBarTexture1,
+      ReputationXPBarTexture2,
+      ReputationXPBarTexture3,
+      ReputationWatchBar.StatusBar.WatchBarTexture0,
+      ReputationWatchBar.StatusBar.WatchBarTexture1,
+      ReputationWatchBar.StatusBar.WatchBarTexture2,
+      ReputationWatchBar.StatusBar.WatchBarTexture3,
+      StanceBarLeft,
+      StanceBarMiddle,
+      StanceBarRight,
+      -- arena frames
+      ArenaEnemyFrame1Texture,
+      ArenaEnemyFrame2Texture,
+      ArenaEnemyFrame3Texture,
+      ArenaEnemyFrame4Texture,
+      ArenaEnemyFrame5Texture,
+      ArenaEnemyFrame1SpecBorder,
+      ArenaEnemyFrame2SpecBorder,
+      ArenaEnemyFrame3SpecBorder,
+      ArenaEnemyFrame4SpecBorder,
+      ArenaEnemyFrame5SpecBorder,
+      ArenaEnemyFrame1PetFrameTexture,
+      ArenaEnemyFrame2PetFrameTexture,
+      ArenaEnemyFrame3PetFrameTexture,
+      ArenaEnemyFrame4PetFrameTexture,
+      ArenaEnemyFrame5PetFrameTexture,
+      ArenaPrepFrame1Texture,
+      ArenaPrepFrame2Texture,
+      ArenaPrepFrame3Texture,
+      ArenaPrepFrame4Texture,
+      ArenaPrepFrame5Texture,
+      ArenaPrepFrame1SpecBorder,
+      ArenaPrepFrame2SpecBorder,
+      ArenaPrepFrame3SpecBorder,
+      ArenaPrepFrame4SpecBorder,
+      ArenaPrepFrame5SpecBorder,
+      -- panes
+      CharacterFrameTitleBg,
+      CharacterFrameBg,
+      ObjectiveTrackerBlocksFrame.QuestHeader.Background,
+      -- minimap
+      MinimapBorder,
+      MinimapBorderTop,
+      MiniMapTrackingButtonBorder,
+      MiniMapLFGFrameBorder,
+      MiniMapBattlefieldBorder,
+      MiniMapMailBorder,
+      -- raid frame
+      CompactRaidFrameContainerBorderFrameBorderTopLeft,
+      CompactRaidFrameContainerBorderFrameBorderTop,
+      CompactRaidFrameContainerBorderFrameBorderTopRight,
+      CompactRaidFrameContainerBorderFrameBorderRight,
+      CompactRaidFrameContainerBorderFrameBorderBottomRight,
+      CompactRaidFrameContainerBorderFrameBorderBottom,
+      CompactRaidFrameContainerBorderFrameBorderBottomLeft,
+      CompactRaidFrameContainerBorderFrameBorderLeft,
+      select(1, MinimapZoomIn:GetRegions()),
+      select(3, MinimapZoomIn:GetRegions()),
+      select(1, MinimapZoomOut:GetRegions()),
+      select(3, MinimapZoomOut:GetRegions()),
+      QueueStatusMinimapButtonBorder,
+      select(1, TimeManagerClockButton:GetRegions())
+    }) do
     v:SetVertexColor(0.33, 0.33, 0.33);
   end
 
   for i, v in pairs({
-    select(2, TimeManagerClockButton:GetRegions())
-  }) do
+      select(2, TimeManagerClockButton:GetRegions())
+    }) do
     v:SetVertexColor(1.0, 1.0, 1.0);
   end
 
   for i, v in pairs({
-    TargetFrameNameBackground,
-    FocusFrameNameBackground
-  }) do
+      TargetFrameNameBackground,
+      FocusFrameNameBackground
+    }) do
     v:SetTexture('Interface\\TargetingFrame\\UI-StatusBar');
   end
 
   for i, v in pairs({
-    PlayerName,
-    PlayerLevelText,
-    TargetFrameTextureFrameName,
-    TargetFrameToTTextureFrameName,
-    FocusFrameToTTextureFrameName,
-    PartyMemberFrame1Name,
-    PartyMemberFrame2Name,
-    PartyMemberFrame3Name,
-    PartyMemberFrame4Name,
-  }) do
+      PlayerName,
+      PlayerLevelText,
+      TargetFrameTextureFrameName,
+      TargetFrameToTTextureFrameName,
+      FocusFrameTextureFrameName,
+      FocusFrameToTTextureFrameName,
+      PartyMemberFrame1Name,
+      PartyMemberFrame2Name,
+      PartyMemberFrame3Name,
+      PartyMemberFrame4Name,
+    }) do
     v:SetVertexColor(1.0, 1.0, 1.0);
     v.SetVertexColor = function () end
   end
 
   for i, v in pairs({
-    PartyMemberFrame1PetFrameName,
-    PartyMemberFrame2PetFrameName,
-    PartyMemberFrame3PetFrameName,
-    PartyMemberFrame4PetFrameName,
-  }) do
+      PartyMemberFrame1PetFrameName,
+      PartyMemberFrame2PetFrameName,
+      PartyMemberFrame3PetFrameName,
+      PartyMemberFrame4PetFrameName,
+    }) do
     v:Hide();
     v.Show = function() end
   end
